c7e37eeb6186338db6206c776f8aa79e85315969
00012026 <91652129+ujavokhir@users.noreply.github.com>
2021-12-01 20:54:43 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+def fancy_hello_world():
+    fancy_string = """
+───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+───█▒▒░░░░░░░░░▒▒█───
+────█░░█░░░░░█░░█────
+─▄▄──█░░░▀█▀░░░█──▄▄─
+█░░█─▀▄░░░░░░░▄▀─█░░█
+"""
+    return fancy_string
+
+print(fancy_hello_world())
